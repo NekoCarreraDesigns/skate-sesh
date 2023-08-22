09f3331e3c9e95cbb2d42b1f5911b52a2c3a079f
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-22 14:55:51 -0600

    added navbar to app

--- skate-sesh/src/App.js
+++ skate-sesh/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
       </Routes>
