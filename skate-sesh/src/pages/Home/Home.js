a03deb11cbff0f4db328478a9f785c1e3b5139f0
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-22 15:02:48 -0600

    switched images around

--- skate-sesh/src/pages/Home/Home.js
+++ skate-sesh/src/pages/Home/Home.js
@@ -13,12 +13,15 @@ const Home = () => {
         tricks.
       </p>
       <img
-        src='https://images.unsplash.com/photo-1544364493-4ff9efeabce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fHNrYXRlYm9hcmRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-        alt='skater'></img>
-      <img
+        className='skate-image'
         src='https://images.unsplash.com/photo-1507738471757-ab1675ad2a89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODh8fHNrYXRlYm9hcmRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
         alt='skater'></img>
       <img
+        className='skate-image'
+        src='https://images.unsplash.com/photo-1544364493-4ff9efeabce3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fHNrYXRlYm9hcmRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+        alt='skater'></img>
+      <img
+        className='skate-image'
         src='https://images.unsplash.com/photo-1573691138222-741a6db847d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fHNrYXRlYm9hcmRpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
         alt='skater'></img>
       <p className='home-page-paragraph'>
