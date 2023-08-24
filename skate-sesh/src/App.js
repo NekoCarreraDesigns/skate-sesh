212311d3dfa89d1f3fb2ae4f5a9b8241371e49ae
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-24 16:34:03 -0600

    added sessions to App

--- skate-sesh/src/App.js
+++ skate-sesh/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./pages/Main/Main";
+import Sessions from "./pages/Sessions/Sessions";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/main' element={<Main />}></Route>
+        <Route path='/sessions' element={<Sessions />}></Route>
       </Routes>
     </>
   );
