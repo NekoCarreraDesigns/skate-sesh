fff29498003afd487c14efe959a71048a3814c8f
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-25 16:25:35 -0600

    added some funcionality and fixed button error

--- skate-sesh/src/pages/Sessions/Sessions.js
+++ skate-sesh/src/pages/Sessions/Sessions.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./Sessions.css";
 import Header from "../../components/header/Header";
 
@@ -6,9 +6,23 @@ const Sessions = () => {
   const options = ["normal", "nollie", "switch", "fakie"];
   const initialSelectedOptions = Array(options.length).fill(options[0]);
   const [selectedOption, setSelectedOption] = useState(initialSelectedOptions);
-  const [completedTricks, setCompletedTricks] = useState(0);
+  const [completedTricks, setTotalCompletedTricks] = useState(0);
   const [totalTriedTricks, setTotalTriedTricks] = useState(0);
   const [completionPercentage, setCompletionPercentage] = useState(0);
+
+  useEffect(() => {
+    const savedCompletionPercentage = localStorage.getItem(
+      "completionPercentage"
+    );
+    if (savedCompletionPercentage) {
+      setCompletionPercentage(parseFloat(savedCompletionPercentage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("completionPercentage", completionPercentage);
+    console.log(completionPercentage);
+  }, [completionPercentage]);
 
   const calculateCompletionPercentage = () => {
     if (totalTriedTricks === 0) {
@@ -22,13 +36,23 @@ const Sessions = () => {
       document.getElementById("totalTriedInput").value,
       10
     );
+
     const totalLanded = parseInt(
       document.getElementById("totalLandedInput").value,
       10
     );
-    setTotalTriedTricks(totalTried);
-    setCompletedTricks(totalLanded);
+
+    const newCompletionPercentage =
+      totalTried === 0 ? 0 : (totalLanded / totalTried) * 100;
+
+    setCompletionPercentage(newCompletionPercentage);
+    // setTotalTriedTricks(totalTried);
+    // setTotalCompletedTricks(totalLanded);
     setCompletionPercentage(calculateCompletionPercentage());
+  };
+
+  const saveToDashboard = () => {
+    alert(`Completion percentage saved ${completionPercentage}%`);
   };
 
   const handleOptionChange = (event, index) => {
@@ -46,11 +70,25 @@ const Sessions = () => {
           <input
             id='totalTriedInput'
             className='trick-input'
-            placeholder='how many kickflips did you do?'></input>
+            placeholder='how many kickflips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             id='totalLandedInput'
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -61,15 +99,36 @@ const Sessions = () => {
           />
           <p className='trick-div-paragraph'>{selectedOption[0]}</p>
           <button onClick={handleCalculateClick}>Calculate</button>
-          <p>Total Completed Tricks: {completedTricks}</p>
-          <p>Total Tried Tricks: {totalTriedTricks}</p>
-          <p>Completion Percentage: {completionPercentage}%</p>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many heelflips did you do?'></input>
+            placeholder='how many heelflips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -79,12 +138,37 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 1)}
           />
           <p className='trick-div-paragraph'>{selectedOption[1]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many shuvit flips did you do?'></input>
+            placeholder='how many shuvit flips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -94,12 +178,37 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 2)}
           />
           <p className='trick-div-paragraph'>{selectedOption[2]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many hardflips did you do?'></input>
+            placeholder='how many hardflips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -109,16 +218,41 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 3)}
           />
           <p className='trick-div-paragraph'>{selectedOption[3]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
         </div>
       </div>
       <div className='trick-div-container'>
         <div className='trick-div-box'>
           <input
             className='trick-input'
-            placeholder='how many shuvit heelflips did you do?'></input>
+            placeholder='how many shuvit heelflips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -128,12 +262,37 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 4)}
           />
           <p className='trick-div-paragraph'>{selectedOption[4]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many shuvits did you do?'></input>
+            placeholder='how many shuvits did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -143,12 +302,37 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 5)}
           />
           <p className='trick-div-paragraph'>{selectedOption[5]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many tre flips did you do?'></input>
+            placeholder='how many tre flips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -158,12 +342,37 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 6)}
           />
           <p className='trick-div-paragraph'>{selectedOption[6]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
           <input
             className='trick-input'
-            placeholder='how many laser flips did you do?'></input>
+            placeholder='how many laser flips did you do?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalTriedTricks(newValue);
+            }}></input>
           <input
             className='trick-input'
-            placeholder='how many did you land?'></input>
+            placeholder='how many did you land?'
+            onChange={(event) => {
+              const newValue =
+                event.target.value.trim() === ""
+                  ? 0
+                  : parseInt(event.target.value, 10);
+              setTotalCompletedTricks(newValue);
+            }}></input>
           <h4 className='trick-div-header'>What stance were they in?</h4>
           <input
             type='range'
@@ -173,6 +382,17 @@ const Sessions = () => {
             onChange={(event) => handleOptionChange(event, 7)}
           />
           <p className='trick-div-paragraph'>{selectedOption[7]}</p>
+          <button onClick={handleCalculateClick}>Calculate</button>
+          <button onClick={saveToDashboard}>Save</button>
+          <p className='trick-div-paragraph'>
+            Total Completed Tricks: {completedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Total Tried Tricks: {totalTriedTricks}
+          </p>
+          <p className='trick-div-paragraph'>
+            Completion Percentage: {completionPercentage}%
+          </p>
         </div>
       </div>
       <div className='trick-div-container'>
