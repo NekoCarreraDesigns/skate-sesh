5b05b673b56557be03206ed45b2a6b7eadea2695
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-26 16:25:13 -0600

    added logic for trick component, to maintain a separate state for each trick, with completion percentage for each stance

---
+++ skate-sesh/src/components/trick/Trick.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./Trick.css";
+
+const Trick = ({
+  trickName,
+  options,
+  initialTotalTried,
+  initialTotalLanded,
+  onCompletionPercentageChange,
+}) => {
+  const [totalTriedTricks, setTotalTriedTricks] = useState(initialTotalTried);
+  const [totalLandedTricks, setTotalLandedTricks] =
+    useState(initialTotalLanded);
+
+  const calculateCompletionPercentage = () => {
+    if (totalTriedTricks === 0) {
+      return 0;
+    }
+    return (totalLandedTricks / totalTriedTricks) * 100;
+  };
+
+  const handleCalculateClick = () => {
+    // Calculate the completion percentage
+    const completionPercentage = calculateCompletionPercentage();
+
+    // Call the callback function provided by the parent component to update its state
+    onCompletionPercentageChange(completionPercentage);
+  };
+
+  return (
+    <div className='trick-div-box'>
+      <h4 className='trick-div-header'>
+        {" "}
+        Trick Name: {options} {trickName}
+      </h4>
+      <label>How many did you try?</label>
+      <input
+        className='trick-input'
+        trickName={trickName}
+        options={options}
+        placeholder='how many did you try?'
+        value={totalTriedTricks}
+        onChange={(event) => {
+          // Handle blank input as 0
+          const newValue =
+            event.target.value.trim() === ""
+              ? 0
+              : parseInt(event.target.value, 10);
+          setTotalTriedTricks(newValue);
+        }}
+      />
+      <br />
+      <label>How many did you land?</label>
+      <input
+        className='trick-input'
+        placeholder='how many did you land?'
+        value={totalLandedTricks}
+        onChange={(event) => {
+          // Handle blank input as 0
+          const newValue =
+            event.target.value.trim() === ""
+              ? 0
+              : parseInt(event.target.value, 10);
+          setTotalLandedTricks(newValue);
+        }}
+      />
+      <button onClick={handleCalculateClick}>Calculate</button>
+      <p>Total Completed Tricks: {totalLandedTricks}</p>
+      <p>Total Tried Tricks: {totalTriedTricks}</p>
+      <p>Completion Percentage: {calculateCompletionPercentage()}%</p>
+    </div>
+  );
+};
+
+export default Trick;
