6fc9f31e1b475bdef5f9bb2d4991cfad293bcec8
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-26 16:26:21 -0600

    added routes for slides and grinds

--- skate-sesh/src/App.js
+++ skate-sesh/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./pages/Main/Main";
 import Sessions from "./pages/Sessions/Sessions";
+import Grind from "./pages/Sessions/Grind";
+import Slides from "./pages/Sessions/Slide";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/main' element={<Main />}></Route>
         <Route path='/sessions' element={<Sessions />}></Route>
+        <Route path='/sessions/grind-tricks' element={<Grind />}></Route>
+        <Route path='/sessions/slide-tricks' element={<Slides />}></Route>
       </Routes>
     </>
   );
