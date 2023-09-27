d15b7cf9d8fc3ea31e18f5910bfa75c2d9305bcb
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-27 15:34:45 -0600

    added tricknav component

--- skate-sesh/src/components/TrickNav.js
+++ skate-sesh/src/components/TrickNav.js
@@ -10,6 +10,16 @@ const TrickNav = () => {
         <a href='/sessions/grind-tricks'>Grind Tricks</a>
         <br />
         <a href='/sessions/slide-tricks'>Slide Tricks</a>
+        <br />
+        <a href='/sessions/manual-tricks'>Manual Tricks</a>
+        <br />
+        <a href='/sessions/rotation-tricks'>Rotation Tricks</a>
+        <br />
+        <a href='/sessions/grab-tricks'>Grab Tricks</a>
+        <br />
+        <a href='/sessions/vert-tricks'>Vert Tricks</a>
+        <br />
+        <a href='/sessions/freestyle-tricks'>freestyle Tricks</a>
       </nav>
     </>
   );
