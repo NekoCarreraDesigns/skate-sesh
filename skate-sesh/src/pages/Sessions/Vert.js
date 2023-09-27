46c826d166702d411e846ad8af2918d7262b616d
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-27 15:23:49 -0600

    added code for Vert tricks

---
+++ skate-sesh/src/pages/Sessions/Vert.js
@@ -0,0 +1,296 @@
+import React, { useState } from "react";
+import "./Sessions.css";
+import Trick from "../../components/trick/Trick";
+import Header from "../../components/header/Header";
+import TrickNav from "../../components/TrickNav";
+import RotationDirection from "../../components/rotationDirection/RotationDirection";
+
+const Vert = () => {
+  const options = ["normal", "nollie", "switch", "fakie"];
+  // const initialSelectedOptions = Array(options.length).fill(options[0]);
+
+  // Initialize an array to track completion percentages for each exercise
+  const initialCompletionPercentages = Array(options.length).fill(0);
+
+  // const [selectedOption, setSelectedOption] = useState(initialSelectedOptions);
+  const [completionPercentages, setCompletionPercentages] = useState(
+    initialCompletionPercentages
+  );
+
+  // Function to update the completion percentage for a specific exercise
+  const updateCompletionPercentage = (index, percentage) => {
+    const newCompletionPercentages = [...completionPercentages];
+    newCompletionPercentages[index] = percentage;
+    setCompletionPercentages(newCompletionPercentages);
+  };
+  return (
+    <>
+      <Header />
+      <TrickNav />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='360'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='720'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='900'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='1080'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='1260'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Mctwist'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Rodeo flip'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Kickflip Mctwist'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='kickflip Rodeo'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Sad Plant'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Andrecht'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Miller Flip'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Elguerial'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Miller flip'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Tuck Knee'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Texas Plant'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <Trick
+            key={index}
+            trickName='Body Jar'
+            options={option}
+            initialTotalTried={0}
+            initialTotalLanded={0}
+            onCompletionPercentageChange={(percentage) =>
+              updateCompletionPercentage(index, percentage)
+            }
+          />
+        ))}
+      </div>
+      <RotationDirection />
+      <div className='trick-div-container'>
+        {options.map((option, index) => (
+          <p key={index}>
+            {option} Completion Percentage: {completionPercentages[index]}%
+          </p>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Vert;
