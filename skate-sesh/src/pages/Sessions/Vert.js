958cfd470b3bb7fe9652c2c91cdc67513b3e72c3
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-28 16:25:52 -0600

    added header and made percentage shorter by adding to fixed

--- skate-sesh/src/pages/Sessions/Vert.js
+++ skate-sesh/src/pages/Sessions/Vert.js
@@ -27,6 +27,7 @@ const Vert = () => {
     <>
       <Header />
       <TrickNav />
+      <h1 className='trick-div-header'>Vert Tricks</h1>
       <div className='trick-div-container'>
         {options.map((option, index) => (
           <Trick
@@ -285,7 +286,8 @@ const Vert = () => {
       <div className='trick-div-container'>
         {options.map((option, index) => (
           <p className='trick-paragraph' key={index}>
-            {option} Completion Percentage: {completionPercentages[index]}%
+            {option} Completion Percentage:{" "}
+            {completionPercentages[index].toFixed(2)}%
           </p>
         ))}
       </div>
