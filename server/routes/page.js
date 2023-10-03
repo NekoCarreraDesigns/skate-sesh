6352a114aee699a76c684a1fb4055401addd6391
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-03 14:59:17 -0600

    added route for user registration

---
+++ server/routes/page.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const routes = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
+
+routes.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ username: req.body.username });
+    if (userExists) {
+      res
+        .status(400)
+        .json({ message: "User already exists, try a different username" });
+    }
+
+    const pinkSalt = 13;
+    const hashedPassword = await bcrypt.hash(req.body.password, pinkSalt);
+
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Registration error", error });
+  }
+});
+
+module.exports = routes;
