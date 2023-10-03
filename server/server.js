5ec79795edb2d9ca0a69b2a3a51f7fb1ae83bbde
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-03 15:18:19 -0600

    edited server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
+require("dotenv").config({ path: "./config.env" });
+
+const db = require("./db/connection");
 const PORT = process.env.Port || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+// app.use(require("./routes/page"));
 
 app.listen(PORT, () => {
+  db.connectToServer(function (err, res) {
+    if (err) {
+      res.status(500).send({ message: "internal server error" });
+    } else {
+      db.setUpSession(app);
+    }
+  });
   console.log(`Server is listening on ${PORT}`);
 });
