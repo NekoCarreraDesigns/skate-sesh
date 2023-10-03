ceb58f39c4e9ca06debdc565fc1ffbfe91c63f3b
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-03 15:04:07 -0600

    added user schema for app

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  tricks: [
+    {
+      trickName: String,
+      totalTried: Number,
+      totalLanded: Number,
+    },
+  ],
+});
+
+const User = mongoose.Model("user", userSchema);
+
+module.exports = User;
