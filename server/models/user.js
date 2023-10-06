6d7a5bc0b9bf6b895c94ae2dc92fc1f6f638e5ca
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-06 15:29:18 -0600

    edited schema

--- server/models/user.js
+++ server/models/user.js
@@ -9,11 +9,13 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
 
   username: {
     type: String,
     required: true,
+    unique: true,
   },
 
   password: {
