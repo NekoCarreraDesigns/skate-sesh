4692e4d3b978ea9508f1b50e85ccfe30e56caace
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-06 15:30:18 -0600

    added validations for email and password

--- server/routes/page.js
+++ server/routes/page.js
@@ -5,18 +5,46 @@ const User = require("../models/user");
 
 routes.post("/user-sign-up", async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    const userExists = await User.findOne({ $or: [{ username }, { email }] });
-    if (userExists) {
+    const { firstName, username, email, password } = req.body;
+    const userExists = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(email)) {
+      res.status(400).json({ message: "Invalid email address" });
+      return;
+    }
+
+    if (password.length < 8) {
       res
         .status(400)
-        .json({ message: "User already exists, try a different username" });
+        .json({ message: "Password must be at least 8 characters long" });
+      return;
+    }
+
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasDigit = /\d/.test(password);
+
+    if (!hasUppercase || !hasLowercase || !hasDigit) {
+      res.status(400).json({
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, and one digit",
+      });
+      return;
+    }
+
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+      return;
     }
 
     const pinkSalt = 13;
     const hashedPassword = await bcrypt.hash(password, pinkSalt);
 
     const newUser = new User({
+      firstName,
       username,
       email,
       password: hashedPassword,
